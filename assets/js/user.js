f05111f2e36725b6702e413b3e9b26e64d0a364b
jonCampzar <jonataneder.campos.a@gmail.com>
2024-10-24 14:39:57 -0600

    Pag user

---
+++ assets/js/user.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("btninc").addEventListener("click", function() {
+    window.location.href = "index.html";
+    alert("En breve se le dará respuesta a tu incidencia.");
+  });
+});

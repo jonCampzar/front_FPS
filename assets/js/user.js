319f5e482a9b14fc0bff6454e0946f3f3aaa4a18
jonCampzar <jonataneder.campos.a@gmail.com>
2024-10-24 15:08:09 -0600

    Toast y redireccion

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("btninc").addEventListener("click", function() {
-    window.location.href = "index.html";
-    alert("En breve se le dará respuesta a tu incidencia.");
-  });
+document.getElementById("liveToastBtn").addEventListener("click", function() {
+  $('#liveToast').toast('show'); // Muestra el toast
+  setTimeout(function() {
+    window.location.href = "index.html"; // Redirige después de que el toast desaparezca
+  }, 3000); // Redirige después de 2 segundos
 });
